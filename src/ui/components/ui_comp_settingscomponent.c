c1b6422952efe2e483a28c198891ca039773aae4
kunikata <kunikata@melody.international>
2026-03-30 16:37:23 +0900

    lcd backlight issue fix

--- src/ui/components/ui_comp_settingscomponent.c
+++ src/ui/components/ui_comp_settingscomponent.c
@@ -293,7 +293,7 @@ lv_obj_t *ui_settingsComponent_create(lv_obj_t *comp_parent)
     lv_obj_set_style_pad_bottom(cui_settingsBackLightPanelLabel, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     ui_settingsBackLightPanelSlider = lv_slider_create(cui_settingsBackLightPanel);
-    lv_slider_set_range(ui_settingsBackLightPanelSlider, 10, 255);
+    lv_slider_set_range(ui_settingsBackLightPanelSlider, XTOUCH_BACKLIGHT_SLIDER_MIN, XTOUCH_BACKLIGHT_SLIDER_MAX);
     lv_slider_set_value(ui_settingsBackLightPanelSlider, xTouchConfig.xTouchBacklightLevel, LV_ANIM_OFF);
     lv_obj_set_height(ui_settingsBackLightPanelSlider, 10);
     lv_obj_set_flex_grow(ui_settingsBackLightPanelSlider, 1);
